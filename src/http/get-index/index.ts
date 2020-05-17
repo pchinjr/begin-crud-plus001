8c182c37a50d78d3cffea3de7d8a2ba3dc79e022
Paul Chin Jr <pchinjr@gmail.com>
2020-05-17 18:51:50 -0400

    hello world part 2, now with fetch

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -4,18 +4,17 @@ const URI = {
   local: 'http://localhost:3333',
   staging: 'https://invent-jf9-staging.begin.app/',
   production: 'https://invent-jf9.begin.app/'
-
 }
 
 export async function handler() {
 
-  //const raw = await fetch('https://invent-jf9-staging.begin.app/todos')
-  //const props = await raw.json()
-  //const body = await render(props)
+  const raw = await fetch("invent-jf9-staging.begin.app/todos")
+  const props = await raw.json()
+  const body = await render(props)
   return {
     headers: {
       'content-type': 'text/html; charset=utf8'
     },
-    body: 'hello world'
+    body: 'body'
   }
 }

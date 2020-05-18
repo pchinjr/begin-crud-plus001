2c97af262b16dcbf84cc954ecabd918d9bdc7eaf
Paul Chin Jr <pchinjr@gmail.com>
2020-05-17 22:22:19 -0400

    more urls

--- src/http/get-index/todo.tsx
+++ src/http/get-index/todo.tsx
@@ -8,7 +8,7 @@ export function Todo(){
 
   // GET all todos
   async function init () {
-    await fetch('/todos', {
+    await fetch('https://invent-jf9-staging.begin.app/todos', {
       credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json'
